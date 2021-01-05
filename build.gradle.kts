7e3acbc3e33012ab03152f7a47cee0c41c075da5
sya-ri <sya79lua@gmail.com>
2021-01-05 11:13:15 +0900

    プロジェクトを作成

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.21"
+    id("com.github.johnrengelman.shadow") version "6.1.0"
+    application
+}
+
+group = "com.github.syari.ss.texture.gitclient"
+
+repositories {
+    mavenCentral()
+    maven("https://repo.eclipse.org/content/groups/releases/")
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation("org.eclipse.jgit:org.eclipse.jgit:5.10.0.202012080955-r")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.withType<ShadowJar> {
+    classifier = null
+    destinationDirectory.set(file("jars"))
+}
+
+application {
+    mainClassName = "$group.MainKt"
+}
---
+++ src/main/kotlin/com/github/syari/ss/texture/gitclient/main.kt
@@ -0,0 +1,5 @@
+package com.github.syari.ss.texture.gitclient
+
+fun main() {
+    println("Hello")
+}
